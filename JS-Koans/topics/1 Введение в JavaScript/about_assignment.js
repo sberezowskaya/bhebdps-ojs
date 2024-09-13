c7d57caaacd26b597d1be91be94f2e099eb371f7
sberezowskaya <143963198+sberezowskaya@users.noreply.github.com>
2024-09-13 17:19:37 +0300

    Реализовано первое задание

--- JS-Koans/topics/1 Введение в JavaScript/about_assignment.js
+++ JS-Koans/topics/1 Введение в JavaScript/about_assignment.js
@@ -1,7 +1,7 @@
 
 describe("About Assignment (about_assignment.js)", function() {
   it("local variables", function() {
-    let temp = FILL_ME_IN;
+    let temp = 1;
     // Assign a value to the variable temp
     expect(temp).toBe(1);
   });
@@ -9,8 +9,7 @@ describe("About Assignment (about_assignment.js)", function() {
   it("global variables", function() {
     temp = 1; // Not using let is an example. Always use let in practise.
     // global variables are assigned to the window object
+    window.FILL_ME_IN = 1
     expect(window.FILL_ME_IN).toBe(temp);
   });
 });
-
-
